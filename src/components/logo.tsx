88246636977e70d5a09bd14a5b1866ad8d227946
Brandaxia <brandaxia@gmail.com>
2025-09-01 15:40:17 +0000

    mejoralo al diseñoque se vea mas intuitivi mas profesional mejor diseño

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -12,28 +12,9 @@ export function Logo() {
         strokeLinecap="round"
         strokeLinejoin="round"
       >
-        {/* Robot */}
-        <path d="M14.5 12.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
-        <path d="M12 15v4" />
-        <path d="M15 19h-6" />
-        <rect x="9" y="4" width="6" height="8" rx="1" />
-        {/* Robot Grad Cap */}
-        <path d="M8 8.5l4-1.5 4 1.5" />
-        <path d="M12 7v1.5" />
-
-        {/* Human */}
-        <circle cx="6" cy="6" r="2" />
-        <path d="M6 8v5" />
-        <path d="M3 17l2-5 2 5" />
-        <path d="M2 13h8" />
-        {/* Human Grad Cap */}
-        <path d="M2 5.5l4-1.5 4 1.5" />
-        <path d="M6 4v1.5" />
-        
-        {/* Diploma */}
-        <path d="M18 20c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z"/>
-        <path d="M18 16V8a2 2 0 0 1 2-2h2"/>
-        <path d="M18 8a2 2 0 0 0-2-2h-2"/>
+        <path d="M12 2L2 7l10 5 10-5-10-5z" />
+        <path d="M2 17l10 5 10-5" />
+        <path d="M2 12l10 5 10-5" />
       </svg>
       <span className="text-xl font-bold font-headline tracking-tight">
         Ainsophic Academy
